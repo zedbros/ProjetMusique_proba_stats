309cca2252e6dd1e8146c03dae3ce72cbdac9aa8
Benno Weber <bennopaul.weber@gmail.com>
2026-01-03 17:35:06 +0100

    optimize weigts function for k-nn

--- algo.jl
+++ algo.jl
@@ -66,6 +66,43 @@ begin
         score /= length(testdata)
         return score
     end
+
+    function generate_possibilities(array1, array2, array3)
+        n = length(array1)
+        # For each index, create a vector of the three options
+        options = [ [array1[i], array2[i], array3[i]] for i in 1:n ]
+
+        # Use Iterators.product to get all combinations
+        result = [vcat(t...) for t in Iterators.product(options...)]
+        
+        return vec(result)
+    end
+
+    function optimize_weights(base_weights, data, labels, testdata, testlabels, k, iterations = 50, multiplier = 2)
+        if iterations == 0
+            return base_weights
+        end
+
+        weights_mult = base_weights .* multiplier
+        weights_div = base_weights ./ multiplier
+        
+        candidate_weight_sets = generate_possibilities(weights_div,base_weights,weights_mult)
+
+        best_weights = base_weights
+        best_score = correctness(base_weights, data, labels, testdata, testlabels, k)
+        
+        for weights in candidate_weight_sets
+            score = correctness(weights, data, labels, testdata, testlabels, k)
+            if score > best_score
+                best_score = score
+                best_weights = weights
+            end
+        end
+        println(best_weights)
+        println(best_score)
+
+        return optimize_weights(best_weights, data, labels, testdata, testlabels, k, iterations -1, multiplier / 2 + 1)
+    end
         
     #data = [datapoint,datapoint,datapoint]
     #labels = [rock, metal, rock]
@@ -120,7 +157,8 @@ begin
         acc = correctness(weights, data, labels, data, labels, k)
         print(acc)
 
-
+        print(optimize_weights(weights, data, labels, data, labels, k))
+        
         estim = k_nn(weights,data,labels,point,k)
 
         x = [d[1] for d in data]
